bd2380a02018556e5aec042df44cfd579ad38b03
Angel Alexander <angale22@gmail.com>
2020-07-31 16:58:45 -0500

    added textareas to rows

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ var NowMoment = moment();
 var displayMoment = document.getElementById('currentDay');
 displayMoment.innerHTML = NowMoment.format('LLLL')
 
+
 $(":button").addClass("saveBtn");
+
+//text area
+textArea = document.createElement("textarea")
+$(".col-8").append(textArea);
+textArea.setAttribute("style", "width:100%;")
